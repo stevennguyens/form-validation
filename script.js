59a5cc81dcc2c2c6aeeda9113590ea689052516e
stevennguyens <stevennguyyenz@gmail.com>
2022-07-24 11:06:08 -0700

    DONE

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
-function createPhoneNumber(numbers){
-    let phone = `${numbers[0]}`
+const form = document.getElementById('form');
 
-    return phone
+const email = document.getElementById('email');
+const emailError = document.querySelector('#email + span.error');
+
+const country = document.getElementById('country');
+const countryError = document.querySelector('#country + span.error');
+
+const zipcode = document.getElementById('zipcode');
+const zipcodeError = document.querySelector('#zipcode + span.error');
+
+const password = document.getElementById('password');
+const passwordError = document.querySelector('#password + span.error');
+
+const passwordCon = document.getElementById('password-con');
+
+
+form.addEventListener('submit', (e) => {
+    if (!email.validity.valid) {
+        showError(email, emailError);
+        e.preventDefault();
+    } else {
+        emailError.textContent = '';
+    }
+
+    if (!country.validity.valid) {
+        showError(country, countryError);
+        e.preventDefault();
+    } else {
+        countryError.textContent = '';
+    }
+
+    if (!zipcode.validity.valid) {
+        showError(zipcode, zipcodeError);
+        e.preventDefault();
+    } else {
+        zipcodeError.textContent = '';
+    }
+
+    if (!password.validity.valid) {
+        showError(password, passwordError);
+        e.preventDefault();
+    } else if (!(passwordCon.validity.valid) || (password.value !== passwordCon.value)) {
+        passwordError.textContent = `Passwords do not match`
+        e.preventDefault();
+    } else {
+        passwordError.textContent = '';
+    }
+})
+
+function showError(e, err) {
+    if (e.validity.valueMissing) {
+        err.textContent = `Enter ${e.id}`;
+    } else if (e.validity.typeMismatch) {
+        err.textContent = `This is not a(n) ${e.id}`
+    } else if (e.validity.tooShort || e.validity.tooLong) {
+        if (e.validity.tooShort && e.validity.tooLong) {
+            err.textContent = `Your ${e.id} should be ${e.minLength} characters long`
+        } else {
+            err.textContent = `Your ${e.id} should be between ${e.minLength} and ${e.maxLength} characters long`
+        }
+    }
+    err.className = 'error'
 }
 
-let numbers = [1,2,3,4,5,6,7,8,9,0]
-console.log(createPhoneNumber(numbers))
